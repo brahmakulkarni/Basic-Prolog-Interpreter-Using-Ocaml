0739b0564d15e230b24dd671aef486d4195ae120
Rathin Bhargava <rathin7@gmail.com>
2020-06-04 13:21:31 +0530

    Working Code, not fully tested

--- Pseudo_Final/search.ml
+++ Pseudo_Final/search.ml
@@ -1,25 +1,4 @@
-type constant =
-  | NUM of int
-  | IDEN of string
-
-type expr = 
-  | VAR of string
-  | CONST of constant
-  | FUNC of string * expr list
-
-type pair_expr = expr * expr
-
-let rec string_of_expr expr =
-  match expr with 
-  | CONST(IDEN(x)) | VAR(x) -> x
-  | CONST(NUM(x)) -> (string_of_int x)
-  | FUNC(name,list1) -> name ^ "(" ^ (string_of_list list1) ^ ")"
-and string_of_list list1 = 
-match list1 with
-| [] -> ""
-| [h] -> (string_of_expr h)
-| h :: t -> (string_of_expr h) ^ ", " ^ (string_of_list t)
-
+open Unified 
 
 type right = 
   | LEAF of Unified.expr
@@ -34,9 +13,9 @@ type rule =
 let rec right_to_list right = 
   match right with 
   | LEAF(x) -> [x]
-  | OR(x,y) -> (List.append x y)
-  | AND(x,y) -> (List.append x y)
-  | NOT(x) -> [x]
+  | OR(x,y) -> (List.append (right_to_list x) (right_to_list y))
+  | AND(x,y) -> (List.append (right_to_list x) (right_to_list y))
+  | NOT(x) -> (right_to_list x)
 
 let rec string_of_right right = 
   match right with
@@ -51,23 +30,16 @@ let rec string_of_rule rule =
   | NODE(x,y) ->
     (Unified.string_of_expr x) ^ " :- " ^ (string_of_right y) 
 
-let debug () = 
-  let a = (Unified.unifier (VAR("X")) (VAR("X"))) in 
-  let b,c = a in
-  Printf.printf "Value: %b\n" b
-
-let _ = debug ()
-
 let rules = [
   HEAD(FUNC("f",[CONST(IDEN("a"))])) ;
   HEAD(FUNC("f",[CONST(IDEN("b"))])) ;
 
   HEAD(FUNC("g",[CONST(IDEN("a"))])) ;
-  HEAD(FUNC("f",[CONST(IDEN("b"))])) ;
+  HEAD(FUNC("g",[CONST(IDEN("b"))])) ;
 
   HEAD(FUNC("h",[CONST(IDEN("b"))])) ;
 
-  NODE(FUNC("h",[VAR("X")]),AND(AND(LEAF(FUNC("f",[VAR("X")])),LEAF(FUNC("g",[VAR("X")]))),LEAF(FUNC("h",[VAR("X")])))) ;
+  NODE(FUNC("k",[VAR("X")]),AND(AND(LEAF(FUNC("f",[VAR("X")])),LEAF(FUNC("g",[VAR("X")]))),LEAF(FUNC("h",[VAR("X")])))) ;
 ]
 
 (* let search_rules query rules =  *)
@@ -93,14 +65,14 @@ and replace_list list1 var value =
 let rec replace_var_hash expr var_expr = 
   match expr with
   | VAR(x) -> 
-    if((Hashtbl.find_opt x) = None) then VAR(x) else (Hashtbl.find x)
+    if((Hashtbl.find_opt var_expr x) = None) then VAR(x) else (Hashtbl.find var_expr x)
   | CONST(x) -> CONST(x)
   | FUNC(x,y) -> FUNC(x,(replace_var_hash_list y var_expr))
 and replace_var_hash_list list1 var_expr = 
   match list1 with
   | [] -> []
   | h::t -> 
-    (replace_var_hash h) :: (replace_var_hash_list t var_expr)
+    (replace_var_hash h var_expr) :: (replace_var_hash_list t var_expr)
 
 let rec has_var expr = 
   match expr with 
@@ -111,70 +83,75 @@ and has_list_var expr list1 =
   match list1 with
   | [] -> false
   | h :: t -> 
-    if (has_var h) true
+    if (has_var h) then true
     else (has_list_var expr t)
 
 
-
-let search_rule_list query goal_list rules = 
-  let search_hash = Hashtbl.create 1000 in
-  match rules with 
-  | [] -> false,search_hash
+let rec search_rule_list query goal_list sub_rules rules = 
+  Printf.printf "Query: %s Goals: %s\n" (string_of_expr query) (string_of_list goal_list);
+  let empty_hash = Hashtbl.create 1000 in
+  match sub_rules with 
+  | [] -> false,empty_hash
   | h :: t ->
     match h with
     | HEAD(x) ->
       let flag,unified_hash = (Unified.unifier x query) in
-      if (flag = false) then (search_list query t) 
+      if (flag = false) then (search_rule_list query goal_list t rules) 
       else 
         let new_goal_list = (replace_var_hash_list goal_list unified_hash) in
-        (search_goal_list new_goal_list rules )
+        let flag_goal,goal_hash = (search_goal_list new_goal_list unified_hash rules rules) in
+        if(flag_goal = false) then (search_rule_list query goal_list t rules)
+        else flag_goal,goal_hash 
         
-      (* else flag,unified_hash *)
-    | NODE(x,y) ->
-      let flag,unified_hash = (Unified.unifier x query) in
-      if (flag = false) then (search_list query t) 
+    | NODE(head,tail) ->
+      let flag,unified_hash = (Unified.unifier head query) in
+      if (flag = false) then (search_rule_list query goal_list t rules) 
       else 
-        let flag_tail,tail_hash = (search_right right rules) in
-        if(flag_tail = false) then (search_list query t)
+        let flag_tail,tail_hash = (search_right tail empty_hash rules rules) in
+        Printf.printf "Query: %s Head: %s Tail: %s flag_tail: %b\n" (string_of_expr query) (string_of_expr head) (string_of_right tail) flag_tail;
+        if(flag_tail = false) then (search_rule_list query goal_list t rules)
         else
-          let new_head = (replace_var_hash x tail_hash) in
+          let new_head = (replace_var_hash head tail_hash) in
           let new_flag,new_unified_hash = (Unified.unifier query new_head) in
-          if(new_flag = false) then Printf.printf "DUMB SHITE ERROR. THIS SHOULD NEVER OCCUR\n";
-          new_flag,new_unified_hash
+          (* if(new_flag = false) then Printf.printf "DUMB SHITE ERROR. THIS SHOULD NEVER OCCUR\n"; *)
+          (*new_flag is true because flag is true*)
+          let new_goal_list = (replace_var_hash_list goal_list new_unified_hash) in
+          let new_flag_goal, new_goal_hash = (search_goal_list new_goal_list new_unified_hash rules rules) in
+          if(new_flag_goal = false) then (search_rule_list query goal_list t rules)
+          else new_flag_goal,new_goal_hash 
+          
 
-and search_goal_list goal_list rules = 
+and search_goal_list goal_list orig_hash sub_rules rules = 
   match goal_list with 
-  | [] -> 
-  | h :: t -> (search_rule_list h t rules)
-and search_right right rules = 
+  | [] -> true,orig_hash
+  | h :: t -> 
+    let flag_tail,tail_hash = (search_rule_list h t rules rules) in
+    (*Unionify the hash tables*)
+    if(flag_tail = true) then 
+    begin
+      Hashtbl.iter (fun x y -> Hashtbl.add orig_hash x y) tail_hash;
+      true,orig_hash
+    end
+    (*Doesn't really matter which hash_table we return*)
+    else flag_tail,tail_hash 
+
+and search_right right empty_hash sub_rules rules = 
   let goal_list = (right_to_list right) in
-  search_goal_list goal_list rules
-
-(* search_list query rules 
-seasrc *)
-(* let rec search_tail right rules = 
-  match rules with
-   *)
-(* let rec solve_query query rules = 
+  search_goal_list goal_list empty_hash rules rules
 
 
-and iter_unify query rules = 
-  let var_exp = Hashtbl.create 1000 in
-  match rules with 
-  | [] -> false,var_exp
-  | h::t -> 
-    match h with 
-    | HEAD(x) -> 
+let test_rule_list () = 
+  let flag,hash = (search_rule_list query [] rules rules) in
+  Hashtbl.iter (fun x y -> if((has_var y)) then () else Printf.printf "%s = %s\n" x (string_of_expr y)) hash;
+  Printf.printf "%b\n" flag
 
-    |    
-    let x,y = Unified.unifier query  *)
 
-let testcases_replace = [
+(* let testcases_replace = [
   FUNC("a",[FUNC("f",[VAR("Y")])]),"Y",CONST(NUM(1)) ;
   FUNC("a",[FUNC("f",[VAR("Y")])]),"Y",FUNC("g",[CONST(NUM(1))])  ;
-]
+] *)
 
-let print_rule x = 
+(* let print_rule x = 
   Printf.printf "%s\n" ((string_of_rule x) ^ ".")
 
 let test_all () = 
@@ -185,8 +162,15 @@ let tester_replace a =
   Printf.printf "Expr:%s Var:%s Value:%s Final:%s\n" (string_of_expr x) y (string_of_expr z) (string_of_expr (replace_var_expr x y z))
 
 let test_replace () = 
-  List.iter tester_replace testcases_replace
+  List.iter tester_replace testcases_replace *)
 
+(* let debug () = 
+  let a = (Unified.unifier (VAR("X")) (VAR("X"))) in 
+  let b,c = a in
+  Printf.printf "Value: %b\n" b
+
+let _ = debug () *)
 
 (* let _  = test_all () *)
-let _ = test_replace ()
+(* let _ = test_replace () *)
+let _ = test_rule_list ()
